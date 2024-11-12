60b84a1bc894865444184f30744a98436b121cf0
andr-33 <andree.serranourraca@gmail.com>
2024-11-12 15:00:57 +0100

    ft: basic structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import PersonalDataView from "./views/personalData";
+import UploadFileView from "./views/uploadFile";
+
+const App = () =>{
 
   return (
-    <div className="bg-slate-400">
-      Hola mundo
+    <div className="w-full flex items-center flex-col bg-green-300">
+      <h1>CV Manager / NetBees</h1>
+      <div className="w-[1000px] flex flex-row flex-grow h-screen">
+        <PersonalDataView />
+        <UploadFileView />
+      </div>
     </div>
   )
-}
+};
 
 export default App
