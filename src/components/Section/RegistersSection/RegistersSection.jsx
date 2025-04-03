1cba761bc25ed700f2ac86bbe75bee1d17d8c0c6
andr-33 <andree.serranourraca@gmail.com>
2025-04-03 12:08:40 +0200

    feature: create user profile, crate their own sidebar and fetch user main information

---
+++ src/components/Section/RegistersSection/RegistersSection.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const RegistersSection = () => {
+    return(
+        <Box>
+            <Typography>Seccion de candidaturas</Typography>
+        </Box>
+    );
+}
+
+export default RegistersSection;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,14 +1,42 @@
 import { useEffect, useState } from "react";
+import { Box, Skeleton, Typography, useTheme } from "@mui/material";
 import { useAuth } from "../contexts/AuthContext/AuthContext";
-import { NotificationProvider, useNotification } from "../contexts/NotificationContext/NotificationContext"; 
+import { NotificationProvider, useNotification } from "../contexts/NotificationContext/NotificationContext";
 import axios from "axios";
 import SlideUpNotification from "../components/Notification/SlideUpNotification/SlideUpNotification";
+import UserSideBar from "../components/Sidebar/UserSidebar/UserSidebar";
+import ImageAvatar from "../components/Avatar/ImageAvatar/ImageAvatar";
+import RegistersSection from "../components/Section/RegistersSection/RegistersSection";
+
+const InformationLoadingSkeletons = () => {
+    return (
+        <>
+            {Array.from({ length: 3 }).map((_, index) => (
+                <Skeleton
+                    key={index}
+                    variant="text"
+                    sx={{
+                        fontSize: index === 0 ? '1.25rem' : '1rem',
+                        width: index === 0 ? '200px' : '150px'
+                    }}
+                />
+            ))}
+        </>
+    );
+};
 
 const UserProfilePage = () => {
-    const [userInfo, setUserInfo]=useState();
+    const [userInfo, setUserInfo] = useState(null);
+    const [expanded, setExpanded] = useState(false);
+    const [activeSection, setActiveSection] = useState('registers');
+    const theme = useTheme();
     const { accessToken } = useAuth();
     const { notification, closeNotification, updateNotification, openNotification } = useNotification();
-    
+
+    const sectionComponents = {
+        registers: <RegistersSection />,
+    };
+
     useEffect(() => {
         const fetchUserInformation = async () => {
             try {
@@ -20,7 +48,7 @@ const UserProfilePage = () => {
                 setUserInfo(response.data);
                 console.log(response.data);
             } catch (error) {
-                console.error('Error: ',error.message);
+                console.error('Error: ', error.message);
                 updateNotification("No pudimos obtener tu información", 'error');
                 openNotification();
             }
@@ -30,8 +58,62 @@ const UserProfilePage = () => {
 
     return (
         <>
+            <Box sx={{
+                display: 'flex',
+                backgroundColor: theme.palette.background.default,
+                position: 'relative',
+                pl: '70px'
+            }}>
+                <UserSideBar
+                    expanded={expanded}
+                    setExpanded={setExpanded}
+                    setActiveSection={setActiveSection}
+                />
 
-            <SlideUpNotification 
+                <Box sx={{ flexGrow: 1, p: 3 }}>
+                    <Box sx={{
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        gap: 2
+                    }}>
+                        <ImageAvatar roleId={1} />
+                        {userInfo ? (
+                            <Box>
+                                <Typography
+                                    variant="body1"
+                                    sx={{
+                                        fontWeight: '600',
+                                        fontSize: 30
+                                    }}
+                                >
+                                    {userInfo.nombre} {userInfo.primer_apellido}
+                                </Typography>
+                                {/*<Typography variant="body2" fontWeight={'600'}>Fecha de alta:
+                                    <Typography component={'span'}> {formatDate(userInfo.fecha_alta)}</Typography>
+                                </Typography>*/}
+                                <Typography variant="body2" fontWeight={'600'}>En:
+                                    <Typography component={'span'}> {userInfo.mst_ciudades_id?.nombre}</Typography>
+                                </Typography>
+                                <Typography variant="body2" fontWeight={'600'}>Contacto:
+                                    <Typography component={'span'}> {userInfo.telefono_movil}</Typography>
+                                </Typography>
+                            </Box>
+                        ) : (
+                            <Box>
+                                <InformationLoadingSkeletons />
+                            </Box>
+                        )}
+                    </Box>
+
+                    <Box component='main' sx={{ mt: 2 }}>
+                        {sectionComponents[activeSection]}
+                    </Box>
+                </Box>
+            </Box>
+
+            <SlideUpNotification
                 message={notification.message}
                 open={notification.open}
                 type={notification.type}
