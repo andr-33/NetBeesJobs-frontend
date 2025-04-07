de666e449ee1ee1fe1bf026bfaeba61d2b642ef9
mauro-solerp <javiermauriciosoler@gmail.com>
2025-04-07 10:04:35 +0200

    refactor: enhance CategoryCard component

--- src/components/Card/CategoryCard/CategoryCard.jsx
+++ src/components/Card/CategoryCard/CategoryCard.jsx
@@ -1,23 +1,29 @@
-import { Paper, Box, Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom"
+import { Paper, Box, Typography, useTheme } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const CategoryCard = ({ imageSrc, title, subtitle }) => {
     const navigate = useNavigate();
+       const theme = useTheme();
 
     return (
         <Paper
-            elevation={3}
-            component={'div'}
+            elevation={5}
+            component="div"
             sx={{
                 textAlign: "center",
                 padding: "20px",
                 backgroundColor: (theme) => theme.palette.background.paper,
-                borderRadius: "10px",
-                width: "200px",
+                borderRadius: "15px",
                 margin: "10px",
-                cursor: "pointer"
+                cursor: "pointer",
+                transition: "all 0.3s ease-in-out",
+                boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.2)",
+                "&:hover": {
+                    transform: "scale(1.05)",
+                    boxShadow: "0px 8px 25px #ffd60a",
+                },
             }}
-            onClick={()=> navigate('/pagina-principal')}
+            onClick={() => navigate("/pagina-principal")}
         >
             <Box
                 component="img"
@@ -26,10 +32,15 @@ const CategoryCard = ({ imageSrc, title, subtitle }) => {
                 sx={{
                     width: 80,
                     height: 80,
+                    filter: "drop-shadow(0px 0px 8px #ffd60a",
                 }}
             />
-            <Typography variant="h6">{title}</Typography>
-            <Typography variant="subtitle2">{subtitle}</Typography>
+            <Typography variant="h6" sx={{ fontWeight: "bold", color: theme.palette.primary.dark }}>
+                {title}
+            </Typography>
+            <Typography variant="subtitle2" sx={{ color: "#666" }}>
+                {subtitle}
+            </Typography>
         </Paper>
     );
 };
