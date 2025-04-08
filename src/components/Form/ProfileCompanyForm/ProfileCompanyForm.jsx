2c4a4e60dd4a4af9bda76338e6ef4748cb8ca3a5
andr-33 <andree.serranourraca@gmail.com>
2025-04-08 11:57:45 +0200

    fix: fiz some issues

--- src/components/Form/ProfileCompanyForm/ProfileCompanyForm.jsx
+++ src/components/Form/ProfileCompanyForm/ProfileCompanyForm.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button, Grid2 as Grid, Typography } from "@mui/material";
+import { Box, Typography, Button, Grid2 as Grid } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useImageProfile } from "../../../contexts/ImageProfileContext/ImageProfileContext";
 import { useAuth } from "../../../contexts/AuthContext/AuthContext";
@@ -12,23 +12,23 @@ import { motion } from "framer-motion";
 import { useTheme } from "@mui/material";
 
 const INITIAL_VALUES = {
-    nombre: '',
-    acronimo: '',
-    mst_comunidades_id: 7,
-    mst_provincias_id: 28,
-    mst_ciudades_id: '',
-    image: ''
+  nombre: '',
+  acronimo: '',
+  mst_comunidades_id: 7,
+  mst_provincias_id: 28,
+  mst_ciudades_id: '',
+  image: ''
 };
 
 const ProfileCompanyForm = ({
-    editMode,
-    handleCloseModal
+  editMode,
+  handleCloseModal
 }) => {
   const [formValues, setFormValues] = useState(INITIAL_VALUES);
   const [loading, setLoading] = useState(false);
   const [showPlans, setShowPlans] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [hovering, setHovering] = useState(false);
+  const isSubmitting = false;
   const navigate = useNavigate();
   const { selectedImage } = useImageProfile();
   const { accessToken } = useAuth();
@@ -77,9 +77,8 @@ const ProfileCompanyForm = ({
         }
       );
       console.log("Respuesta del servidor al crear la empresa:", response.data);
-      console.log("ID de la empresa creada:", response.data[0].emp_empresas_id);
-
-      if (!response.data || !response.data[0] || !response.data[0].emp_empresas_id) {
+      
+      if (!response.data[0]?.emp_empresas_id) {
         throw new Error("No se pudo obtener el ID de la empresa de la respuesta del servidor");
       }
 
@@ -92,13 +91,11 @@ const ProfileCompanyForm = ({
 
   const handleFreePlan = async () => {
     setLoading(true);
-    const baseUrl = window.location.origin;
 
     try {
       const companyId = await handleCreateCompany();
 
       if (!companyId) {
-        console.error("companyId is undefined");
         updateNotification(
           "No se pudo obtener el ID de la empresa creada",
           "error"
@@ -118,15 +115,9 @@ const ProfileCompanyForm = ({
       }, 2000);
     } catch (error) {
       console.error("Error al crear la empresa:", error);
-      if (error.response) {
-        updateNotification(
-          error.response.data.error || "Error al crear el perfil con el plan gratuito",
-          "error"
-        );
-      } else {
-        updateNotification("Error de conexión con el servidor", "error");
-      }
+      updateNotification( "Error al crear el perfil con el plan gratuito", "error");
       openNotification();
+    } finally {
       setLoading(false);
     }
   };
@@ -156,6 +147,7 @@ const ProfileCompanyForm = ({
       console.error("Error iniciando pago:", error);
       updateNotification("Error iniciando el pago con Stripe", "error");
       openNotification();
+    } finally {
       setLoading(false);
     }
   };
@@ -164,52 +156,52 @@ const ProfileCompanyForm = ({
     event.preventDefault();
     setLoading(true);
 
-    try{
-        const response = await axios.put('/api/companies/update-profile', formValues, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            }
-        });
-
-        updateNotification('Perfil actualizado con exito!', 'success');
-        openNotification();
-    } catch (error) {
-        console.error('Error updating company:', error);
-        updateNotification('Lo sentimos, algo ha salido mal', 'error');
-        openNotification();
-    } finally {
-        setLoading(false);
-        setFormValues(INITIAL_VALUES);
-        handleCloseModal();
-    }
-};
-
-useEffect(() => {
-    const fetchCompanyData = async () => {
-        try {
-            const response = await axios.get('/api/companies/company-information', {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                }
-            });
-            const companyData = response.data;
-            const mappedData = {
-                nombre: companyData.nombre,
-                acronimo: companyData.acronimo,
-                mst_comunidades_id: companyData.mst_ciudades_id.mst_provincias_id.mst_comunidades_id.mst_comunidades_id,
-                mst_provincias_id: companyData.mst_ciudades_id.mst_provincias_id.mst_provincias_id,
-                mst_ciudades_id: companyData.mst_ciudades_id.mst_ciudades_id,
-            }
-            setFormValues(mappedData);
-        } catch (error) {
-            console.error('Error fetching company data:', error);
-            updateNotification("No pudimos obtener la información", 'error');
-            openNotification();
+    try {
+      const response = await axios.put('/api/companies/update-profile', formValues, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
         }
+      });
+
+      updateNotification('Perfil actualizado con exito!', 'success');
+      openNotification();
+    } catch (error) {
+      console.error('Error updating company:', error);
+      updateNotification('Lo sentimos, algo ha salido mal', 'error');
+      openNotification();
+    } finally {
+      setLoading(false);
+      setFormValues(INITIAL_VALUES);
+      handleCloseModal();
+    }
+  };
+
+  useEffect(() => {
+    const fetchCompanyData = async () => {
+      try {
+        const response = await axios.get('/api/companies/company-information', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          }
+        });
+        const companyData = response.data;
+        const mappedData = {
+          nombre: companyData.nombre,
+          acronimo: companyData.acronimo,
+          mst_comunidades_id: companyData.mst_ciudades_id.mst_provincias_id.mst_comunidades_id.mst_comunidades_id,
+          mst_provincias_id: companyData.mst_ciudades_id.mst_provincias_id.mst_provincias_id,
+          mst_ciudades_id: companyData.mst_ciudades_id.mst_ciudades_id,
+        }
+        setFormValues(mappedData);
+      } catch (error) {
+        console.error('Error fetching company data:', error);
+        updateNotification("No pudimos obtener la información", 'error');
+        openNotification();
+      }
     };
 
     if (editMode) fetchCompanyData();
-},[]);
+  }, []);
 
   if (isSubmitting) {
     return (
@@ -236,60 +228,61 @@ useEffect(() => {
   }
 
   return (
-        <Box
-            component="form"
-            onSubmit={editMode ? handleSubmitUpdate : handleSubmit}
-        >
-            <Typography
-                variant="h6"
-                sx={{
-                    mb: 1
-                }}
-            >
-                {editMode ? "Datos de la empresa: " : "Sobre tu empresa:"}
-            </Typography>
-            <CustomTextFieldWithIcon
-                label="Nombre de la empresa"
-                name="nombre"
-                value={formValues.nombre}
-                onChange={handleOnChange}
-                required
-            />
-            <CustomTextFieldWithIcon
-                label="Acrónimo"
-                name="acronimo"
-                value={formValues.acronimo}
-                onChange={handleOnChange}
-                required
-            />
-            <OptionPicker
-                urlData='/api/master/communities'
-                label="Comunidad Autonoma"
-                name="mst_comunidades_id"
-                value={formValues.mst_comunidades_id}
-                onChange={handleOnChange}
-                idKey='mst_comunidades_id'
-                labelKey='nombre_corto'
-            />
-            <OptionPicker
-                urlData={`/api/master/communities/${formValues.mst_comunidades_id}/provinces`}
-                label="Provincia"
-                name="mst_provincias_id"
-                value={formValues.mst_provincias_id}
-                onChange={handleOnChange}
-                idKey='mst_provincias_id'
-                labelKey='nombre'
-            />
-            <OptionPicker
-                urlData={`/api/master/provinces/${formValues.mst_provincias_id}/cities`}
-                label="Ciudad"
-                name="mst_ciudades_id"
-                value={formValues.mst_ciudades_id}
-                onChange={handleOnChange}
-                idKey='mst_ciudades_id'
-                labelKey='nombre'
-            />
-            {!showPlans && (
+    <Box
+      component="form"
+      onSubmit={editMode ? handleSubmitUpdate : handleSubmit}
+    >
+      <Typography
+        variant="h6"
+        sx={{
+          mb: 1
+        }}
+      >
+        {editMode ? "Datos de la empresa: " : "Sobre tu empresa:"}
+      </Typography>
+      <CustomTextFieldWithIcon
+        label="Nombre de la empresa"
+        name="nombre"
+        value={formValues.nombre}
+        onChange={handleOnChange}
+        required
+      />
+      <CustomTextFieldWithIcon
+        label="Acrónimo"
+        name="acronimo"
+        value={formValues.acronimo}
+        onChange={handleOnChange}
+        required
+      />
+      <OptionPicker
+        urlData='/api/master/communities'
+        label="Comunidad Autonoma"
+        name="mst_comunidades_id"
+        value={formValues.mst_comunidades_id}
+        onChange={handleOnChange}
+        idKey='mst_comunidades_id'
+        labelKey='nombre_corto'
+      />
+      <OptionPicker
+        urlData={`/api/master/communities/${formValues.mst_comunidades_id}/provinces`}
+        label="Provincia"
+        name="mst_provincias_id"
+        value={formValues.mst_provincias_id}
+        onChange={handleOnChange}
+        idKey='mst_provincias_id'
+        labelKey='nombre'
+      />
+      <OptionPicker
+        urlData={`/api/master/provinces/${formValues.mst_provincias_id}/cities`}
+        label="Ciudad"
+        name="mst_ciudades_id"
+        value={formValues.mst_ciudades_id}
+        onChange={handleOnChange}
+        idKey='mst_ciudades_id'
+        labelKey='nombre'
+      />
+
+      {!showPlans && (
         <LoaderButton text="Crear perfil de empresa" loading={loading} />
       )}
 
@@ -351,7 +344,7 @@ useEffect(() => {
                 </motion.span>
               </Typography>
               <Typography sx={{ my: 2 }}>
-                Publica ofertas de trabajo y accede a herramientas exclusivas.
+                Publica ilimitadas ofertas de empleo.
               </Typography>
               <Button
                 fullWidth
@@ -374,8 +367,8 @@ useEffect(() => {
           </Grid>
         </Grid>
       )}
-        </Box>
-    );
+    </Box>
+  );
 };
 
 export default ProfileCompanyForm;
