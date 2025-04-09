93299b318399135536b14783d0d532d1317595bb
mauro-solerp <javiermauriciosoler@gmail.com>
2025-04-09 10:57:53 +0200

    refactor: update WelcomePageSection layout and styling for improved structure

--- src/components/Section/WelcomePageSection/WelcomePageSection.jsx
+++ src/components/Section/WelcomePageSection/WelcomePageSection.jsx
@@ -1,16 +1,20 @@
 import { Box } from '@mui/material';
 
-const WelcomePageSection = ({children, sx}) => {
+const WelcomePageSection = ({ children, sx }) => {
     return (
-        <Box
-            sx={{
-                minHeight: '100vh',
-                display: 'flex',
-                alignItems: 'center',
-                ...sx
-            }}
-        >
-            {children}
+        <Box my={6}>
+            <Box
+                sx={{
+                    minHeight: '83vh',
+                    display: 'flex',
+                    borderRadius: 25,
+                    alignItems: 'center',
+                    ...sx
+                }}
+            >
+                {children}
+            </Box>
+
         </Box>
     );
 };
